cf041d7d6138e5d91557dbe1001f38c044bf82f4
jesusdelafuente <jesusdelafureyesjdlfr@gmail.com>
2022-05-02 17:08:44 -0600

    Consultas fáciles completas, Avance en Intermedias

--- Query2 Consultas FACILES.sql
+++ Query2 Consultas FACILES.sql
@@ -19,3 +19,56 @@ SELECT MIN(Monto) FROM tblTarjetaRegalo)
 SELECT IdTarjetaRegalo,FechaVencimiento,Monto FROM tblTarjetaRegalo
 WHERE (Monto > 10000) AND (Monto < 20000)
 ORDER BY Monto ASC
+
+-- 4.Conocer el o los Articulos con menor precio de la Tienda de Juguetes 
+SELECT NombreArt,Precio AS 'Menor precio' FROM tblArticulos WHERE Precio = (
+SELECT MIN(Precio) FROM tblArticulos)
+
+-- 5.Conocer nombres de los Clientes, cuyo nombre empiece con la letra E o S. ordenelos alfabeticamente
+SELECT NombreCliente, Cedula FROM tblClientes
+WHERE NombreCliente LIKE 'E%' OR 
+NombreCliente LIKE 'S%'
+ORDER BY NombreCliente ASC
+
+-- 6.Conocer los apellidos de los Clientes que terminen con la letra Z o A. ordenelos des alfabeticamente
+SELECT ApellidoCliente, Cedula FROM tblClientes
+WHERE ApellidoCliente LIKE '%Z' OR 
+ApellidoCliente LIKE '%A'
+ORDER BY ApellidoCliente DESC
+
+-- 7.Conocer las tarjetas de regalo cuyo monto sea mayor a 30000 pero menor o igual a 40000. ordenar de menor a mayor
+SELECT IdTarjetaRegalo,FechaVencimiento,Monto FROM tblTarjetaRegalo
+WHERE (Monto > 30000) AND (Monto <= 40000) 
+ORDER BY Monto ASC
+
+-- 8.Conocer los Articulos cuyos nombres contengan las letras F o J o L. ordenelos de mas caro a mas barato
+SELECT NombreArt, Codigo, Precio FROM tblArticulos
+WHERE NombreArt LIKE '%F%' OR 
+NombreArt LIKE '%J%' OR
+NombreArt LIKE '%L%'
+ORDER BY Precio DESC
+
+-- 9.Conocer los nombres de los Clientes cuyos nombres su segunda letra sea una L o su nombre contenga cualquier vocal
+SELECT NombreCliente, Cedula FROM tblClientes
+WHERE (NombreCliente LIKE '_L%') 
+			OR
+(NombreCliente LIKE '%A%' AND
+NombreCliente LIKE '%E%' AND
+NombreCliente LIKE '%I%' AND
+NombreCliente LIKE '%O%' AND
+NombreCliente LIKE '%U%')
+
+-- 10.Conocer el nombre del destinatario cuyo nombre su segunda letra sea A o I o S y la cuarta sea E o O  y termine en vocal 
+SELECT Nombredestinatario FROM tblTarjetaRegalo
+WHERE (Nombredestinatario LIKE '_A%' OR
+Nombredestinatario LIKE '_I%' OR
+Nombredestinatario LIKE '_S%') 
+			AND
+(Nombredestinatario LIKE '___E%' OR
+Nombredestinatario LIKE '___O%') 
+			AND
+(Nombredestinatario LIKE '%A' OR
+Nombredestinatario LIKE '%E' OR
+Nombredestinatario LIKE '%I' OR
+Nombredestinatario LIKE '%O' OR
+Nombredestinatario LIKE '%U')
--- Query3 Consultas INTERMEDIAS.sql
+++ Query3 Consultas INTERMEDIAS.sql
@@ -3,3 +3,18 @@ Base de Datos I Proyecto Tienda de Juguetes.
 db_TiendaJuguetes
 CONSULTAS INTERMEDIAS
 */
+
+-- 1.Conocer los precios de los articulos donde esten entre 20000 y 30000 o 35000 y 45000
+SELECT IdArticulo, NombreArt, Precio FROM tblArticulos
+WHERE (Precio BETWEEN 20000 AND 30000) OR
+(Precio BETWEEN 35000 AND 45000)
+
+-- 2.Conocer las tarjetas de regalo que venzan entre abril y junio del 2022. ordenelos de menor a mayor
+SELECT IdTarjetaRegalo, FechaVencimiento, Monto FROM tblTarjetaRegalo
+WHERE (FechaVencimiento BETWEEN '04/2022' AND '06/2022')
+ORDER BY FechaVencimiento ASC
+
+-- 3.Conocer las tarjeta de regalo que se vendieron por un monto entre 3000 y 7000. ordenelos de mayor a menor
+SELECT IdTarjetaRegalo, Monto FROM tblTarjetaRegalo
+WHERE (Monto BETWEEN 3000 AND 7000)
+ORDER BY Monto DESC
