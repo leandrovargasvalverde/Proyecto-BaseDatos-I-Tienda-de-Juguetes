0d647119b4d23d9ff36aa83720807a332789cdc2
Ana Solis <87107890+AnaSolis29@users.noreply.github.com>
2022-05-03 14:38:41 -0600

    Integrantes

--- Insert Clientes.sql
+++ Insert Clientes.sql
@@ -1,5 +1,9 @@
 /*
 Base de Datos I Proyecto Tienda de Juguetes.
+Integrantes: Leandro Valverde 
+             Ana Solis
+	     Jesus de la Fuente
+	     Sebastian Moreno 
 db_TiendaJuguetes
 INSERTAR DATOS A LA TABLA CLIENTES
 */
@@ -16,13 +20,13 @@ INSERT INTO tblClientes(
 			Telefono,
 			Correo)
 VALUES
-(1,'Leandro Martin','Vargas Valverde','117850521','800 metros norte del templo de Desamparaditos de Puriscal,San José','62166673','leandro9700@gmail.com'),
+(1,'Leandro Martin','Vargas Valverde','117850521','800 metros norte del templo de Desamparaditos de Puriscal,San JosÃ©','62166673','leandro9700@gmail.com'),
 (2,'Oscar Gerardo','Mora Mora','206950784','Del cruce de Josefina en Mercedez Norte de Puriscal, 100 metros al oeste hacia San Juan Puriscal','71080791','oscarmora2006@gmail.com'),
-(3,'Brandom Esteban ','Salazar Ureña','119250493','400 metros norte de la iglesia católica de Desamparaditos,Puriscal','89568993','brandom.salazar.urena@gmail.com'),
+(3,'Brandom Esteban ','Salazar UreÃ±a','119250493','400 metros norte de la iglesia catÃ³lica de Desamparaditos,Puriscal','89568993','brandom.salazar.urena@gmail.com'),
 (4,'Ronald Jose','Jimenez Perez','117710593','Calle los Arley en Cerbatana, Mercedez Sur, Puriscal','63336896','ronaldjimenezp17@gmail.com'),
 (5,'Maria Isabel','Vargas Valverde','115480147','Villa Ceci, San Rafael Arriba, Desamparados','84107689','belvargasv@gmail.com'),
 (6,'Juan Jose','Alvarez Lopez','118550847','1km al Sur del tajo la palma , Mercedez Sur, Puriscal','85266721','juan19262501@gmail.com'),
-(7,'Jose Pablo','Villalobos Valverde','108180507','700 metros norte de la iglesia católica de Desamparaditos,Puriscal','61928401','pablojo142020@gmail.com'),
+(7,'Jose Pablo','Villalobos Valverde','108180507','700 metros norte de la iglesia catÃ³lica de Desamparaditos,Puriscal','61928401','pablojo142020@gmail.com'),
 (8,'Kevin','Jimenez Mendoza','114900561','Del Colegio Tecnico de Puriscal 600 metros al suroeste','83105648','kevinm19@gmail.com'),
 (9,'Gabriel Andres','Cascante Herrera','114800491','Del cementerio de San Antonio 100 metros al sur, porton negro','89505292','gabocas0118@gmail.com'),
 (10,'Jose Pablo','Quiros Gomez','112470763','De la sucursal de la CCSS en Puriscal 200 metros al norte, casa a mano izquierda','88394442','jose1163@gmail.com'),
@@ -33,14 +37,14 @@ VALUES
 (15,'Wendeyvelin Aida ','Fallas Mendoza','113430649','De la escuela de la trinidad de Moravia 3000 al sur calle sin salida','89980926','w.wen88@hotmail.com'),
 (16,'Cristina','Aguilar', '118370542', 'Salitrillos, Puriscal','87156821', 'krisaguilarjime@gmail.com'),
 (17,'Adriana','Aguilar', '1172600302', 'Grifo Alto, Puriscal','8603557','aguilarjimenezadriana@gmail.com'),
-(18,'Jose','Ureña', '207770863', 'Mercedes Sur, Puriscal','86348556', 'urea.jose@hotmail.com'),
+(18,'Jose','UreÃ±a', '207770863', 'Mercedes Sur, Puriscal','86348556', 'urea.jose@hotmail.com'),
 (19,'Luis','Porras', '117680200', 'Poro, Puriscal','85540624', 'luisporras24@gmail.com'),
-(20,'Allison','Rojas', '116680424', 'Hatillo, San José', '86243515', 'allison-01021@hotmail.com'),
+(20,'Allison','Rojas', '116680424', 'Hatillo, San JosÃ©', '86243515', 'allison-01021@hotmail.com'),
 (21,'Yerlin','Madrigal', '117590031', 'San Miguel, Desamparados', '85054834', 'yerlinmr23@gmail.com'),
 (22,'Zet','Porras Fallas', '114880157', 'La Palma, Mercedes Sur, Puriscal', '87563489', 'zetporras@gmail.com'),
-(23,'Zack','Porras Ureña', '123870878', 'La Palma, Mercedes Sur, Puriscal', '87347644', 'zackporrasu@gmail.com'),
-(24,'Higinio','Ureña Jiménez', '105770400', 'La Legua, Mercedes Sur, Puriscal', '67543278', 'higinio1403@gmail.com'),
-(25,'Isabel','Ureña Aguilar', '116560042', 'La Palma, Mercedes Sur, Puriscal', '87453699', 'isabelurena@hotmail.com'),
+(23,'Zack','Porras UreÃ±a', '123870878', 'La Palma, Mercedes Sur, Puriscal', '87347644', 'zackporrasu@gmail.com'),
+(24,'Higinio','UreÃ±a JimÃ©nez', '105770400', 'La Legua, Mercedes Sur, Puriscal', '67543278', 'higinio1403@gmail.com'),
+(25,'Isabel','UreÃ±a Aguilar', '116560042', 'La Palma, Mercedes Sur, Puriscal', '87453699', 'isabelurena@hotmail.com'),
 (26,'Alicia','Aguilar Guadamuz', '107520642', 'La Legua, Mercedes Sur, Puriscal', '89734526', 'aliciaag@gmail.com'),
 (27,'Omar','Solano Alvarado', '109470796', 'Tres Rios, Cartago', '83742215', 'osolanoa@gmail.com'),
 (28,'Ever','Mejia Paniagua', '601810920', 'Santo Domingo, Heredia', '87556321', 'evermejia@gmail.com'),
@@ -53,7 +57,7 @@ VALUES
 (35,'Maria Jose','Jimenez Delgado', '117250578', 'Estero De Puriscal, 150 metros noroeste del CAIS', '86488520', 'mjose24@gmail.com'),
 (36,'Fabiana','Cespedez Delgado', '118650785', 'Estero De Puriscal, 150 metros noroeste del CAIS', '86963451', 'fabii2507@gmail.com'),
 (37,'Javier','Delgado Solis', '109852588', 'Estero De Puriscal, 150 metros noroeste del CAIS', '83003582', 'javierenrique12@gmail.com'),
-(38,'Jesús','De La fuente Reyes', '118550200', 'Barrio El Carmen, Contiguo a la UNED', '84648776', 'jesusreyes7@outlook.com'),
+(38,'JesÃºs','De La fuente Reyes', '118550200', 'Barrio El Carmen, Contiguo a la UNED', '84648776', 'jesusreyes7@outlook.com'),
 (39,'Violeta','Salas Fernandez', '103890497', 'Barrio El Carmen, Contiguo a la UNED', '88540918', 'violetasalas22@hotmail.com'),
 (40,'Johnny','Chavez Cordero', '108170654', 'Barrio El Carmen, Contiguo a la UNED', '62109431', 'johnnychaves169@gmail.com'),
 (41,'Analive','Salas Fernandez', '103490576', 'Barrio El Carmen, Contiguo al Poli Deportivo', '89176429', 'anasalas47@hotmail.com'),
