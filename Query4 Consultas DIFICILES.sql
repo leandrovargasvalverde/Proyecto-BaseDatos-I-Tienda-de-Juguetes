a2484718e274a9bb6acbf23b6ae34066feff5972
leandrovargasvalverde <leandro9700@gmail.com>
2022-05-02 22:31:55 -0600

    Update Query4 Consultas DIFICILES.sql

--- Query4 Consultas DIFICILES.sql
+++ Query4 Consultas DIFICILES.sql
@@ -34,4 +34,9 @@ SELECT IdFactura, Codigo, TotalNeto FROM tblFacturas
 WHERE TotalNeto < (SELECT AVG(TotalNeto) FROM tblFacturas)
 ORDER BY TotalBruto DESC
 
--- 6.
+-- 6. Sumar y Agrupar los Articulos por Proveedor tomando en cuenta que su Precio es mayor que el promedio de los Articulos, ordénelos de menor a mayor.*/
+SELECT AVG(Precio) FROM tblArticulos
+SELECT SUM(Precio) AS 'GANANCIAS POR PROVEEDOR',IdProveedor FROM tblArticulos
+WHERE Precio > (SELECT AVG(Precio) FROM tblArticulos)
+GROUP BY IdProveedor
+ORDER BY SUM(Precio) ASC
