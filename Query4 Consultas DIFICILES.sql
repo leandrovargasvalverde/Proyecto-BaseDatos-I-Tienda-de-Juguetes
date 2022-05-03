eb9f4198e2a3594abe6626511146ed78cc79eda9
jesusdelafuente <jesusdelafureyesjdlfr@gmail.com>
2022-05-02 22:17:01 -0600

    Actualización

--- Query4 Consultas DIFICILES.sql
+++ Query4 Consultas DIFICILES.sql
@@ -4,24 +4,29 @@ db_TiendaJuguetes
 CONSULTAS DIFICILES
 */
 
---  1.Saber cuales facturas obtuvieron un total bruto mas bajo que el promedio. ordenelos de forma descendente.
+--  1.Saber cuales facturas obtuvieron un total bruto mas bajo que el promedio del total bruto de las facturas. ordenelos de forma descendente.
+SELECT AVG(TotalBruto) AS 'Promedio del Total Bruto de las Facturas' FROM tblFacturas
 SELECT IdFactura, Codigo, TotalBruto FROM tblFacturas
 WHERE TotalBruto < (SELECT AVG(TotalBruto) FROM tblFacturas)
 ORDER BY TotalBruto DESC
 
 -- 2.Conocer cuales facturas tuvieron un total bruto mayor al minimo total neto. ordenelos de forma ascendente.
+SELECT MIN(TotalNeto) AS 'Total Neto de menor precio' FROM tblFacturas
 SELECT IdFactura, Codigo, TotalBruto, TotalNeto FROM tblFacturas
 WHERE TotalBruto > (SELECT MIN(TotalNeto) FROM tblFacturas)
 ORDER BY TotalNeto ASC
 
 -- 3.Conocer las facturas donde el promedio del total bruto fue menor al promedio del total neto. ordenelos de forma descendente.
-SELECT IdFactura, Codigo, TotalBruto, TotalNeto FROM tblFacturas
+SELECT AVG(TotalBruto) AS 'Promedio del Total Bruto de las Facturas' FROM tblFacturas
+SELECT AVG(TotalNeto) AS 'Promedio del Total Neto de las Facturas' FROM tblFacturas
+SELECT IdFactura, Codigo, TotalBruto , TotalNeto FROM tblFacturas
 WHERE (SELECT AVG(TotalBruto) FROM tblFacturas) < (SELECT AVG(TotalNeto) FROM tblFacturas)
 ORDER BY TotalNeto DESC
 
 -- 4.Conocer cuales facturas tuvieron un total neto mayor al total bruto mas alto. ordenelos de forma ascendente.
+SELECT AVG(TotalBruto) AS 'Promedio del Total Bruto de las Facturas' FROM tblFacturas
 SELECT IdFactura, Codigo, TotalBruto, TotalNeto FROM tblFacturas
-WHERE TotalNeto > (SELECT MAX(TotalBruto) FROM tblFacturas)
+WHERE TotalNeto > (SELECT AVG(TotalBruto) FROM tblFacturas)
 ORDER BY TotalNeto ASC
 
 -- 5.Saber cuales facturas obtuvieron un total neto mas bajo que el promedio. ordenelos de forma descendente.
